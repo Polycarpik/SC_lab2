1cb9c755a6f0e060433a1604c297f8d45735d5d3
Polina <apolo3neorama>
2014-04-26 17:57:49 +0300

    obligatoryMath completed

--- src/lab2Files/ObligatoryMath.java
+++ src/lab2Files/ObligatoryMath.java
@@ -8,21 +8,31 @@ import java.util.List;
  */
 public class ObligatoryMath {
 
-    private List<Integer> multiplicities = new LinkedList<Integer>();
-
     public int gcd(int a, int n) {
-        if (a > 1) {
-            multiplicities.add((n - n % a) / a);
-        }
         if (a == 0) {
             return n;
         }
         return gcd(n % a, a);
     }
 
+    private int gcd(int a, int n, List<Integer> multiplicities) {
+        if (a > 1) {
+            multiplicities.add((n - n % a) / a);
+        }
+        if (a == 0) {
+            return n;
+        }
+        return gcd(n % a, a, multiplicities);
+    }
+
+    private List<Integer> getMultiplicities(int a, int n) {
+        List<Integer> multiplicities = new LinkedList<Integer>();
+        gcd(a, n, multiplicities);
+        return multiplicities;
+    }
 
     public int getInverseElement(int number, int module) {
-        gcd(number, module);
+        List<Integer> multiplicities = getMultiplicities(number, module);
         int[] p = new int[multiplicities.size() + 2];
         p[0] = 0;
         p[1] = 1;
@@ -35,11 +45,10 @@ public class ObligatoryMath {
         return p[p.length - 1];
     }
 
-
     public int[] solvingLinearComparisons(int a, int b, int n) {
         int d = gcd(a, n);
+        int[] result = new int[d];
         if (d == 1) {
-            int[] result = new int[1];
             result[0] = (getInverseElement(a, n) * b) % n;
             return result;
         } else {
@@ -47,14 +56,14 @@ public class ObligatoryMath {
                 int a1 = a / d;
                 int b1 = b / d;
                 int n1 = n / d;
-                int[] result = new int[d];
-                for(int i = 0; i<result.length; i++){
-                    result[i] = (((getInverseElement(a1, n1) * b1) % n1) + d*n1)%n;
+                for (int i = 0; i < result.length; i++) {
+                    result[i] = (((getInverseElement(a1, n1) * b1)) + i * n1) % n;
                 }
                 return result;
-            } else { return null;}
+            } else {
+                return null;
+            }
         }
-
     }
 
 
